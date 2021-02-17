d220a60bea7c0347f5a7d2eb7c550b9c304ed549
Paul <hqdrone@gmail.com>
2021-02-17 14:52:26 +0300

    header/hero done

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,5 +1,19 @@
 $(document).ready(function(){
 
-	// ...
+	$(window).scroll(function(){
+		if ($(window).scrollTop() > $('.header').height()) {
+			$('.header').addClass('header_active')
+		} else {
+			$('.header').removeClass('header_active')
+		}
+	})
+
+	$('.header__burger').click(function(){
+		$('.mobile').addClass('mobile_active')
+	})
+
+	$('.mobile__close').click(function(){
+		$('.mobile').removeClass('mobile_active')
+	})
 
 })
